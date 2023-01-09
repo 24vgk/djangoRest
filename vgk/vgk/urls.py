6a4f9c0917fe799f45fc70f1c613f518274db788
andrejkalabin <admin@24vgk.ru>
2023-01-09 11:19:30 +0300

    complete lesson_1

--- vgk/vgk/urls.py
+++ vgk/vgk/urls.py
@@ -19,7 +19,7 @@ from rest_framework.routers import DefaultRouter
 from app.views import AuthorModelViewSet
 
 router = DefaultRouter()
-router.register('authors', AuthorModelViewSet)
+router.register('Users', AuthorModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
